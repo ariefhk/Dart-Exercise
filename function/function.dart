722581f535c7a4291266e4227281f58e7b46ad59
ariefhk <ariefrachmanhakim1001@gmail.com>
2022-06-29 20:40:28 +0700

    add func

---
+++ function/function.dart
@@ -0,0 +1,9 @@
+void main(List<String> args) {
+  var angka1 = 5;
+  var angka2 = 10;
+  tambah(angka1, angka2);
+}
+
+void tambah(num1, num2) {
+  print('hasil dari penjumlahan ${num1} + ${num2}: ${num1 + num2}');
+}
