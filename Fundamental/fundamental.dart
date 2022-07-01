6baba4f4dc7f0c4ca0f83544780577deba31f82d
ariefhk <ariefrachmanhakim1001@gmail.com>
2022-07-01 20:54:58 +0700

    add fundamental strategy dart

---
+++ Fundamental/fundamental.dart
@@ -0,0 +1,49 @@
+
+// Fundamental di dart
+// - Comment
+//   > Single Line
+//   > Multi Line
+// - Variable
+// - Numbers
+//   > int
+//   > double
+//   > toStringAsFixed
+//   > parse
+// - Data Types
+//   > int
+//   > double
+//   > num
+//   > bool
+//   > String
+//   > List
+//   > Map
+//   > Dynamic
+// - String
+//   > Interpolation
+//   > Backslash
+// - Boolean
+//   > true
+//   > false
+// - Operators
+//   > Operator Aritmatika
+//   > Parentheses
+//   > Increment
+//   > Decrement
+//   > Operator Perbandingan
+//   > Operator Logika
+// - Exceptions
+//   > try and catch
+//   > finally
+// - Functions
+//   > Function Parameter
+//   > Optional Parameter
+// - Variable Scope
+// - Constans & Final
+// - Null Safety
+// - Switch & Case
+// - Collections
+//   > Add
+//   > Insert
+//   > Spread Collections
+//   > Set
+//   > Remove
